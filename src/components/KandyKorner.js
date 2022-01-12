ac1d689c1f582d9a636c8812c5a6af8e9dec30d9
Sara Hackle <Shackle@ivytech.edu>
2022-01-12 10:11:21 -0600

    Locations added

--- src/components/KandyKorner.js
+++ src/components/KandyKorner.js
@@ -1,10 +1,13 @@
-
+import { LocationList } from "./locations/LocationList";
 
 export const KandyKorner = () => {
 
   return (
     <>
       <h1>Kandy Korner</h1>
+
+      <h2>Location List</h2>
+      <LocationList />
     </>
   );
 };
