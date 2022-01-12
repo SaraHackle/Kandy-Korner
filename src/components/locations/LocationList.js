b0d07e965602e2d9e0753fab8b4bff180bed29e5
Sara Hackle <Shackle@ivytech.edu>
2022-01-12 10:23:31 -0600

    fixed formatting

--- src/components/locations/LocationList.js
+++ src/components/locations/LocationList.js
@@ -2,25 +2,25 @@ import React, { useState, useEffect } from "react";
 
 export const LocationList = () => {
   const [locations, assignLocations] = useState([]);
-  
+
   useEffect(() => {
-      fetch("http://localhost:8088/locations")
+    fetch("http://localhost:8088/locations")
       .then((res) => res.json())
       .then((locationArray) => {
-          assignLocations(locationArray);
-        });
-    }, 
-    []
-    );
-    
-    return (
-        <>
-    {locations.map((locationObject) => {
-        return (
-            <p key= {`location--${locationObject.id}`}>Address: {locationObject.address}{","} Nashville, TN </p>
-            )
-        })}
-    </>
-)
+        assignLocations(locationArray);
+      });
+  }, []);
 
+  return (
+    <>
+      {locations.map((locationObject) => {
+        return (
+          <p key={`location--${locationObject.id}`}>
+            Address: {locationObject.address}
+            {","} Nashville, TN{" "}
+          </p>
+        );
+      })}
+    </>
+  );
 };
