578e76031b707e0afc26de8550fc1e66008da20c
Sara Hackle <Shackle@ivytech.edu>
2022-01-19 16:33:58 -0600

    formatting

--- src/components/customers/CustomerList.js
+++ src/components/customers/CustomerList.js
@@ -1,24 +1,21 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 
 export const CustomerList = () => {
-    const [customers, assignCustomers] = useState([])
+  const [customers, assignCustomers] = useState([]);
 
-    useEffect(() => {
-        fetch("http://localhost:8088/customers")
-        .then((data) => data.json())
-        .then ((customerArray) => {
-            assignCustomers(customerArray)
-        })
-    },[])
+  useEffect(() => {
+    fetch("http://localhost:8088/customers")
+      .then((data) => data.json())
+      .then((customerArray) => {
+        assignCustomers(customerArray);
+      });
+  }, []);
 
-    return (
-        <div>
-            {customers.map((customerObject) => {
-                return (
-                    <p key={customerObject.id}> 
-                    {customerObject.name}</p>
-                )
-            })}
-        </div> 
-    )
-}
+  return (
+    <div>
+      {customers.map((customerObject) => {
+        return <p key={customerObject.id}>{customerObject.name}</p>;
+      })}
+    </div>
+  );
+};
